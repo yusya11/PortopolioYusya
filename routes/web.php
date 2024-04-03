e64094f83ecae0148d6d67d4be2ad631692c271e
MOHAMMAD HAIDAR YUSYA <143923293+yusya11@users.noreply.github.com>
2024-04-03 16:30:46 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/web', function () {
-    return view('web');
-});
-Route::get('/yusya', function () {
     return view('yusya');
 });
+
